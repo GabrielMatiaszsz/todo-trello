a48342c79a15902658d1b01aac2ecd36091186d3
gabriel matias <gmsilva1@uniara.edu.br>
2024-03-21 00:49:09 -0300

    logo adicionada

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,18 @@
+const MarketingLayout = ({
+    children
+  }: {
+    children: React.ReactNode;
+  }) => {
+    return (
+        <div className="h-full bg-slate-100">
+            {/* navbar */}
+           <main className="pt-40 pb-20 bg-slate-200">
+           {children}
+           </main>
+             {/* footer */}
+        </div>
+      )
+  }
+ 
+
+  export default MarketingLayout;
